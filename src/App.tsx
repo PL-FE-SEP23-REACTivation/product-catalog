0c275003a3aa3c887814668835a35090fe60def6
Marcin Ligmann <marcinligmann2@gmail.com>
2024-02-08 13:59:27 +0100

    added Route * pagenotfound

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,17 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import Phones from './pages/Phones';
+import PageNotFound from './pages/PageNotFound';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Link to="/">
-        Home
-      </Link>
-      <Link to="/phones">
-        Phones
-      </Link>
+      <Link to="/">Home</Link>
+      <Link to="/phones">Phones</Link>
 
       <Routes>
         <Route path="/phones" element={<Phones />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
