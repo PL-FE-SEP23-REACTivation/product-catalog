c218fd3691c88f1f366e2b424e7d2d235225abff
Laura Wronska <laura.wronskaa@gmail.com>
2024-02-09 00:04:07 +0100

    add pagination button component

--- src/components/PaginationButton/PaginationButton.tsx
+++ src/components/PaginationButton/PaginationButton.tsx
@@ -1,26 +1,45 @@
 import classNames from 'classnames';
 import { FC } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
 import './PaginationButton.scss';
 
 type Props = {
   children: React.ReactNode;
   darkBorder?: boolean;
   disabled?: boolean;
+  next?: boolean;
+  prev?: boolean;
 };
 
 export const PaginationButton: FC<Props> = ({
   children,
-  darkBorder,
   disabled,
+  next,
+  prev,
 }) => {
+  let toPage = Number(children);
+  const [searchParams] = useSearchParams();
+  const currentPage = Number(searchParams.get('page'));
+
+  if (next) {
+    toPage = currentPage + 1;
+  }
+
+  if (prev) {
+    toPage = currentPage - 1;
+  }
+
   return (
-    <button
-      disabled={disabled}
+    <Link
+      to={{ search: `?page=${toPage}` }}
       className={classNames('pagination-button', {
-        'pagination-button--dark': darkBorder,
+        'pagination-button--prev': prev,
+        'pagination-button--next': next,
+        disabled: disabled,
+        'pagination-button--is-active': toPage === currentPage,
       })}
     >
       {children}
-    </button>
+    </Link>
   );
 };
