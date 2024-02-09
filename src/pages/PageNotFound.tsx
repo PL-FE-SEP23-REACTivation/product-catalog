057c589b82fae181d43a51748a25608e50b863cb
Bartek Zmuda <bertsu@protonmail.com>
2024-02-09 09:24:15 +0100

    fix/footer-folder

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PageNotFound from '../components/Footer/PageNotFound/PageNotFound';
+import PageNotFound from '../components/PageNotFound/PageNotFound';
 
 const Pagenotfound: React.FC = () => {
   return <PageNotFound />;
