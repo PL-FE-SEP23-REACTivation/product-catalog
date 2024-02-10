ce631ea728fac74452685b3f93ec83abe25b635a
Adrian <adrian.krause.ak@gmail.com>
2024-02-10 17:52:00 +0100

    Connected PhonePage and ProductCard.

--- src/pages/Phones.tsx
+++ src/pages/Phones.tsx
@@ -4,35 +4,121 @@ import { Link } from 'react-router-dom';
 import homeIcon from '../icons/home.svg';
 import rightArrowIcon from '../icons/right-arrow.svg';
 import { Pagination } from '../components/Pagination';
+import { ProductCard } from '../components/ProductCard/ProductCard';
 
 const PhonesPage: React.FC = () => {
   const products = [
     {
-      name: 'product1',
+      id: 1,
+      category: 'phones',
+      itemId: 'apple-iphone-7-32gb-black',
+      name: 'Apple iPhone 7 32GB Black',
+      fullPrice: 400,
+      price: 375,
+      screen: "4.7' IPS",
+      capacity: '32GB',
+      color: 'black',
+      ram: '2GB',
+      year: 2016,
+      image: 'img/phones/apple-iphone-7/black/00.webp',
     },
     {
-      name: 'product2',
+      id: 2,
+      category: 'phones',
+      itemId: 'apple-iphone-7-32gb-black',
+      name: 'Apple-iphone-7-32gb-black',
+      fullPrice: 540,
+      price: 500,
+      screen: "5.5' IPS",
+      capacity: '32GB',
+      color: 'black',
+      ram: '3GB',
+      year: 2016,
+      image: 'img/phones/apple-iphone-7-plus/black/00.webp',
     },
     {
-      name: 'product3',
+      id: 3,
+      category: 'phones',
+      itemId: 'apple-iphone-8-64gb-gold',
+      name: 'Apple iPhone 8 64GB Gold',
+      fullPrice: 600,
+      price: 550,
+      screen: "4.7' IPS",
+      capacity: '64GB',
+      color: 'gold',
+      ram: '2GB',
+      year: 2017,
+      image: 'img/phones/apple-iphone-8/gold/00.webp',
     },
     {
-      name: 'product4',
+      id: 4,
+      category: 'phones',
+      itemId: 'apple-iphone-11-64gb-black',
+      name: 'Apple iPhone 11 64GB Black',
+      fullPrice: 932,
+      price: 880,
+      screen: "6.1' IPS",
+      capacity: '64GB',
+      color: 'black',
+      ram: '4GB',
+      year: 2019,
+      image: 'img/phones/apple-iphone-11/black/00.webp',
     },
     {
-      name: 'product1',
+      id: 1,
+      category: 'phones',
+      itemId: 'apple-iphone-7-32gb-black',
+      name: 'Apple iPhone 7 32GB Black',
+      fullPrice: 400,
+      price: 375,
+      screen: "4.7' IPS",
+      capacity: '32GB',
+      color: 'black',
+      ram: '2GB',
+      year: 2016,
+      image: 'img/phones/apple-iphone-7/black/00.webp',
     },
     {
-      name: 'product1',
+      id: 2,
+      category: 'phones',
+      itemId: 'apple-iphone-7-32gb-black',
+      name: 'Apple-iphone-7-32gb-black',
+      fullPrice: 540,
+      price: 500,
+      screen: "5.5' IPS",
+      capacity: '32GB',
+      color: 'black',
+      ram: '3GB',
+      year: 2016,
+      image: 'img/phones/apple-iphone-7-plus/black/00.webp',
     },
     {
-      name: 'product1',
+      id: 3,
+      category: 'phones',
+      itemId: 'apple-iphone-7-32gb-black',
+      name: 'Apple iPhone 8 64GB Gold',
+      fullPrice: 600,
+      price: 550,
+      screen: "4.7' IPS",
+      capacity: '64GB',
+      color: 'gold',
+      ram: '2GB',
+      year: 2017,
+      image: 'img/phones/apple-iphone-8/gold/00.webp',
     },
     {
-      name: 'product1',
-    },
-    {
-      name: 'product2',
+      id: 4,
+      category: 'phones',
+      itemId: 'apple-iphone-11-64gb-black',
+      name: 'Apple iPhone 11 64GB Black',
+      fullPrice: 932,
+      price: 880,
+      screen: "6.1' IPS",
+      capacity: '64GB',
+      color: 'black',
+      ram: '4GB',
+      year: 2019,
+      image: 'img/phones/apple-iphone-11/black/00.webp',
     },
   ];
   return (
@@ -54,8 +140,8 @@ const PhonesPage: React.FC = () => {
       <div className="phones_count">{`${products.length} models`}</div>
       <div className="phones_cards">
         {products.map((product) => (
-          <div className="phones_cards_item" key={product.name}>
-            {product.name}
+          <div className="phones_cards_item" key={product.id}>
+            <ProductCard product={product} />
           </div>
         ))}
       </div>
