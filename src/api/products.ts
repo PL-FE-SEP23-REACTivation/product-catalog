ebc4fdcfbdec78aa5fa9106fed788132b1b5c147
Marcin Ligmann <marcinligmann2@gmail.com>
2024-02-16 14:58:44 +0100

    added temporary comment to function in api/products.ts

--- src/api/products.ts
+++ src/api/products.ts
@@ -2,6 +2,8 @@ import { DetailedProduct } from '../types/detailedProductType';
 import { Product } from '../types/productType';
 import { client } from '../utils/fetchClient';
 
+//for /phones (better to make it universal and use getProductsByCategorie )
+//(PagePhones)
 export const getPhones = () => {
   return client.get<DetailedProduct[]>('/phones');
 };
@@ -10,46 +12,20 @@ export const getProductByItemId = (id: string) => {
   return client.get<Product>(`/products/${id}`);
 };
 
+//for path /:category (need change route)
+export const getProductsByCategorie = (categorie: string) => {
+  return client.get<Product[]>(`/${categorie}`);
+};
+
+//for /products/:id/recommended (ProductPage)
 export const getProductsRecommended = (id: string) => {
   return client.get<Product[]>(`/products/${id}/recommended`);
 };
 
+//for /:category/:id (ProductPage)
 export const getProductByIdAndCategory = async (
   productId: string,
   category: string
 ) => {
   return await client.get<DetailedProduct>(`/${category}/${productId}`);
 };
-
-// export const addTodo = ({ title, userId, completed }: Omit<Todo, 'id'>) => {
-//   return client.post<Todo>('/todos', { title, userId, completed });
-// };
-
-// export const deleteTodo = (todoId: number) => {
-//   return client.delete(`/todos/${todoId}`);
-// };
-
-// export const updateTodo = (
-//   url: number, data: Partial<Todo>,
-// ): Promise<Todo> => {
-//   return client.patch(`/todos/${url}`, data);
-// };import { Todo } from '../types/Todo';
-// import { client } from '../utils/fetchClient';
-
-// export const getTodos = (userId: number) => {
-//   return client.get<Todo[]>(`/todos?userId=${userId}`);
-// };
-
-// export const addTodo = ({ title, userId, completed }: Omit<Todo, 'id'>) => {
-//   return client.post<Todo>('/todos', { title, userId, completed });
-// };
-
-// export const deleteTodo = (todoId: number) => {
-//   return client.delete(`/todos/${todoId}`);
-// };
-
-// export const updateTodo = (
-//   url: number, data: Partial<Todo>,
-// ): Promise<Todo> => {
-//   return client.patch(`/todos/${url}`, data);
-// };
