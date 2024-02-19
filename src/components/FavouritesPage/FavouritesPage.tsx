d0b4b18c9b0bae86a5a50423fcd88eb2eeb6fc79
Adrian <adrian.krause.ak@gmail.com>
2024-02-20 00:17:56 +0100

    Fix some margins and add breadcrumbs component to favourites page.

--- src/components/FavouritesPage/FavouritesPage.tsx
+++ src/components/FavouritesPage/FavouritesPage.tsx
@@ -1,10 +1,9 @@
 /* eslint-disable max-len */
 import React from 'react';
-import homeIcon from '../../icons/home.svg';
-import rightArrowIcon from '../../icons/right-arrow.svg';
 import './FavouritesPage.scss';
 import { ProductCard } from '../ProductCard/ProductCard';
 import { useFavoritesStore } from '../../storage/FavouritesStore';
+import { Breadcrumbs } from '../Breadcrumbs/Breadcrumbs';
 
 const FavouritesPage: React.FC = () => {
   const favoriteProducts = useFavoritesStore((state) => state.favoriteProducts);
@@ -12,20 +11,10 @@ const FavouritesPage: React.FC = () => {
   return (
     <div className="favourites">
       <div className="favourites__header">
-        <img className="favourites__header-icon" src={homeIcon} alt="home" />
-        <img
-          className="favourites__header-icon"
-          src={rightArrowIcon}
-          alt="arrow"
-        />
-        <div className="favourites__header-category">Favourites</div>
+        <Breadcrumbs path={'Favourites'} />
       </div>
-      <div className="favourites__title">
-        Favourites
-        <div className="favourites__title-count">
-          {favoriteProducts.length} items
-        </div>
-      </div>
+      <div className="favourites__title">Favourites</div>
+      <div className="favourites__count">{favoriteProducts.length} items</div>
       <div className="favourites__content">
         {favoriteProducts.map((product) => (
           <div className="favourites__content-items" key={product.id}>
