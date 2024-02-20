d39948901eddc37ac71611c8e37a34abc66ff0c5
Marcin Ligmann <marcinligmann2@gmail.com>
2024-02-20 13:43:15 +0100

    added debounce to sending searchParams search to url

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,27 +1,36 @@
 import { useSearchParams } from 'react-router-dom';
 import './Search.scss';
 import { useState } from 'react';
+import useDebounce from './debounce';
 
 export const Search: React.FC = () => {
   const [inputValue, setInputValue] = useState('');
   const [searchParams, setSearchParams] = useSearchParams();
-  // const search = searchParams.get('search');
+  const debounce = useDebounce(setSearchParams, 500);
+  const search = searchParams.get('search');
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     const { value } = event.target;
 
     setInputValue(value);
 
-    if (!inputValue) {
+    searchParams.set('search', value);
+
+    if (value === '') {
       searchParams.delete('search');
     }
-    searchParams.set('search', value);
-    setSearchParams(searchParams);
+
+    debounce(searchParams);
   };
 
   return (
     <div className="search">
-      <input type="text" value={inputValue} onChange={handleChange} />
+      <input
+        type="text"
+        value={search || inputValue}
+        placeholder="Search..."
+        onChange={handleChange}
+      />
     </div>
   );
 };
