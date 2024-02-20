b2197f3d2b5ea584d0baf0a12c59686c5f7019fc
Marcin Ligmann <marcinligmann2@gmail.com>
2024-02-20 12:23:13 +0100

    working search input without style

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,22 +1,27 @@
-// import classNames from 'classnames';
-import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-// import { capitalize, getSearchParams } from '../../utils/helpers';
 import './Search.scss';
+import { useState } from 'react';
 
 export const Search: React.FC = () => {
-  const [search, setSearch] = useState('');
-  const [searchParams] = useSearchParams();
-  const param = searchParams.get('search');
-  console.log(param);
+  const [inputValue, setInputValue] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
+  // const search = searchParams.get('search');
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setSearch(event.target.value);
+    const { value } = event.target;
+
+    setInputValue(value);
+
+    if (!inputValue) {
+      searchParams.delete('search');
+    }
+    searchParams.set('search', value);
+    setSearchParams(searchParams);
   };
 
   return (
     <div className="search">
-      <input type="text" value={search} onChange={handleChange} />
+      <input type="text" value={inputValue} onChange={handleChange} />
     </div>
   );
 };
