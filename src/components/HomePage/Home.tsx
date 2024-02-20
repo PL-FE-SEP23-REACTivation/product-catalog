b9ab416e7540c3825244b73ca2590edf23ba7d2f
Bartek Zmuda <bertsu@protonmail.com>
2024-02-20 11:59:38 +0100

    new-product-slider

--- src/components/HomePage/Home.tsx
+++ src/components/HomePage/Home.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Carousel from './Carousel/Carousel';
 import NewProducts from './NewProducts/NewProducts';
 import Categories from './Categories/Categories';
 import HotPrices from './HotPrices/HotPrices';
-
+import { Product } from '../../types/productType';
 import './Home.scss';
 
 const HomePage: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    fetch('https://reactivation.onrender.com/products')
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsError(true);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div className="homePage">
       <div className="title">
@@ -17,7 +34,12 @@ const HomePage: React.FC = () => {
       </div>
 
       <div className="newProducts">
-        <NewProducts />
+        <NewProducts
+          products={products}
+          isLoading={isLoading}
+          isError={isError}
+          title="Brand New Models"
+        />
       </div>
       <div className="category">
         <Categories />
