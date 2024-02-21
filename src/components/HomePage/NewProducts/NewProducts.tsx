aa79e21eeb1f6166bba47aaf5463268b0baef820
Bartek Zmuda <bertsu@protonmail.com>
2024-02-21 15:15:31 +0100

    slider component

--- src/components/HomePage/NewProducts/NewProducts.tsx
+++ src/components/HomePage/NewProducts/NewProducts.tsx
@@ -1,10 +1,6 @@
-import React, { useState } from 'react';
-import './NewProducts.scss';
-import { ProductCard } from '../../ProductCard/ProductCard';
-import arrowLeft from '../../../icons/arrow-left.svg';
-import arrowRight from '../../../icons/arrow-right.svg';
-import { Loader } from '../../Loader/Loader';
+import React from 'react';
 import { Product } from '../../../types/productType';
+import { Slider } from '../Slider/Slider';
 
 interface Props {
   products: Product[];
@@ -19,60 +15,14 @@ const NewProducts: React.FC<Props> = ({
   isError,
   title,
 }) => {
-  const [slideIndex, setSlideIndex] = useState(0);
-
-  const handlePrevClick = () => {
-    setSlideIndex((prevIndex) => Math.max(prevIndex - 1, 0));
-  };
-
-  const handleNextClick = () => {
-    setSlideIndex((prevIndex) => Math.min(prevIndex + 1, products.length - 1));
-  };
-
   return (
-    <section className="newProducts">
-      <div className="newProducts__header">
-        <h1 className="newProducts__header--title">{title}</h1>
-        <div className="newProducts__header--btn">
-          <button
-            className="newProducts__button"
-            type="button"
-            onClick={handlePrevClick}
-            disabled={slideIndex === 0}
-          >
-            <img src={arrowLeft} alt="button-left" />
-          </button>
-          <button
-            className="newProducts__button"
-            type="button"
-            onClick={handleNextClick}
-            disabled={slideIndex === products.length - 4}
-          >
-            <img src={arrowRight} alt="button-left" />
-          </button>
-        </div>
-      </div>
-
-      <div className="newProducts__content">
-        {isLoading && !isError && <Loader />}
-        {!isLoading && isError && (
-          <p>Error: Unable to load data from server!</p>
-        )}
-        {!isLoading && !isError && (
-          <ul
-            className="newProducts__list"
-            // eslint-disable-next-line max-len
-            style={{ transform: `translateX(-${slideIndex * (272 + 16)}px)` }}
-          >
-            {products.map((product) => (
-              <li className="newProducts__item" key={product.id}>
-                <ProductCard product={product} />
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    </section>
+    <Slider
+      products={products}
+      isLoading={isLoading}
+      isError={isError}
+      title={title}
+      className="new-products"
+    />
   );
 };
 
