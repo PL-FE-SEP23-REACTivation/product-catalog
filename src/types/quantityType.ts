cfc15d01aae9c61bbe27470fe894a0d15f796c69
Marcin Ligmann <marcinligmann2@gmail.com>
2024-02-21 14:05:37 +0100

    change in categories component, now using GET /products/quantity endpoint

--- src/types/quantityType.ts
+++ src/types/quantityType.ts
@@ -1,3 +1,14 @@
 export type Quantity = {
   quantity: number;
 };
+
+export type AllQuantity = {
+  accessories: number | undefined;
+  phones: number | undefined;
+  tablets: number | undefined;
+};
+
+export type CategoryCount = {
+  category: string;
+  count: number;
+};
