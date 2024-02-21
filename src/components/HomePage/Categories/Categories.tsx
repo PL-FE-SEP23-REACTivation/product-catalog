38e7d9a6281475dac03498253f43bd9c9bd30e4b
Bartek Zmuda <bertsu@protonmail.com>
2024-02-21 17:17:12 +0100

    added shop by category section on home page, added flex-column on fav-page

--- src/components/HomePage/Categories/Categories.tsx
+++ src/components/HomePage/Categories/Categories.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Categories.scss';
 import { getQuantityByCategory } from '../../../api/products';
 
@@ -6,6 +7,10 @@ const Categories: React.FC = () => {
   const [phones, setPhones] = useState<number>(0);
   const [tablets, setTablets] = useState<number>(0);
   const [accessories, setAccessories] = useState<number>(0);
+
+  const backToTop = () => {
+    window.scrollTo(0, 0);
+  };
 
   useEffect(() => {
     const getProductsQuantity = async () => {
@@ -26,12 +31,81 @@ const Categories: React.FC = () => {
   }, []);
 
   return (
-    <>
-      <h2>Shop by category</h2>
-      <p>{`Mobile phones: ${phones} models`}</p>
-      <p>{`Tablets: ${tablets} models`}</p>
-      <p>{`Mobile phones: ${accessories} models`}</p>
-    </>
+    <section className="category">
+      <div className="category__content">
+        <h1 className="category__title">Shop by category</h1>
+
+        <ul className="category__list" data-cy="categoryLinksContainer">
+          <li className="category__item">
+            <Link
+              to="/phones"
+              className="category__image-link category__image-link--phones"
+              onClick={backToTop}
+            >
+              <img
+                src={process.env.PUBLIC_URL + '/img/category-phones.png'}
+                alt="phones"
+                className="category__image"
+              />
+            </Link>
+
+            <div className="category__name-block">
+              <Link to="/phones" className="category__name" onClick={backToTop}>
+                Mobile phones
+              </Link>
+
+              <span className="category__amount">{`${phones} models`}</span>
+            </div>
+          </li>
+
+          <li className="category__item">
+            <Link
+              to="/tablets"
+              className="category__image-link category__image-link--tablets"
+              onClick={backToTop}
+            >
+              <img
+                src={process.env.PUBLIC_URL + '/img/category-tablets.png'}
+                alt="tablets"
+                className="category__image"
+              />
+            </Link>
+
+            <div className="category__name-block">
+              <Link to="/phones" className="category__name" onClick={backToTop}>
+                Tablets
+              </Link>
+
+              <span className="category__amount">{`${tablets} models`}</span>
+            </div>
+          </li>
+
+          <li className="category__item">
+            <Link
+              to="/accessories"
+              className="category__image-link category__image-link--accessories"
+              onClick={backToTop}
+            >
+              <img
+                src={process.env.PUBLIC_URL + '/img/category-accessories.png'}
+                alt="accessories"
+                className="category__image"
+              />
+            </Link>
+
+            <div className="category__name-block">
+              <Link to="/phones" className="category__name" onClick={backToTop}>
+                Accessories
+              </Link>
+
+              <span className="category__amount">
+                {`${accessories} models`}
+              </span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 };
 
