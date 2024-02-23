8ae1c8a522b88651d68a6c5fad4c0675f1e4ebd0
Marcin Szymański <107401426+szymanski-marcin@users.noreply.github.com>
2024-02-23 02:38:59 +0100

    Theme saved in localStorage

--- src/storage/ThemeStore.ts
+++ src/storage/ThemeStore.ts
@@ -1,11 +1,20 @@
-import create from 'zustand';
+import { create } from 'zustand';
 
 interface ThemeStore {
   darkMode: boolean;
   toggleDarkMode: () => void;
 }
 
-export const useThemeStore = create<ThemeStore>((set) => ({
-  darkMode: false,
-  toggleDarkMode: () => set((state) => ({ darkMode: !state.darkMode })),
-}));
+export const useThemeStore = create<ThemeStore>((set) => {
+  const savedDarkMode = localStorage.getItem('darkMode');
+  const initialDarkMode = savedDarkMode ? JSON.parse(savedDarkMode) : false;
+
+  return {
+    darkMode: initialDarkMode,
+    toggleDarkMode: () => set((state) => {
+      const newDarkMode = !state.darkMode;
+      localStorage.setItem('darkMode', JSON.stringify(newDarkMode));
+      return { darkMode: newDarkMode };
+    }),
+  };
+});
